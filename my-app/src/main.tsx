b96c225725ac04a84c8eb1d5ee26f9952bed6707
Syed Abdullah Ali <syedabdullahali380@gmail.com>
2026-08-09 15:02:03 +0530

    hotfix(type) ignore type check

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./i18n";
 import "./index.css";
-import "./Theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter >
